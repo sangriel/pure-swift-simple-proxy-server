41afb75d8bd2c62e5c0dd87c69d77425f77a0b57
sangriel <39114237+sangriel@users.noreply.github.com>
2024-02-05 22:42:30 +0900

    엑스코드 프로젝트 생성 (#1)

---
+++ SimpleInAppProxyServer/SimpleInAppProxyServer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleInAppProxyServer
+//
+//  Created by sangmin han on 2024/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
